8b0a8475c3c0dd08a98c7c3e8d477952ce973ecf
saifurrahmanproject <s.r.tonoy.p@gmail.com>
2020-10-27 01:34:28 -0400

    Initial Commit

---
+++ SaifurR Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SaifurR Card
+//
+//  Created by Tonoy Rahman on 2020-10-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SaifurR Card/SaifurR_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaifurR_CardApp.swift
+//  SaifurR Card
+//
+//  Created by Tonoy Rahman on 2020-10-27.
+//
+
+import SwiftUI
+
+@main
+struct SaifurR_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
