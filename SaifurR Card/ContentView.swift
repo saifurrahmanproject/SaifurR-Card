fc57a5071f6c89236a4454d762bc6016d27681ce
saifurrahmanproject <s.r.tonoy.p@gmail.com>
2020-10-27 03:22:13 -0400

    add custom font and customize full app.

--- SaifurR Card/ContentView.swift
+++ SaifurR Card/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.28, green: 0.86, blue: 0.98, opacity: 0.75)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                
+                Image("Saifur")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.gray, lineWidth: 5)
+                    )
+                
+                
+                Text("Saifur Rahman Tonoy")
+                    .font(Font.custom("GreatVibes-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.black)
+                Text("Computer Science Engineer")
+                    .foregroundColor(.black).font(.system(size: 20))
+                Divider()
+                InfoView(text: "+1 5142953412", imageName: "phone.bubble.left")
+                InfoView(text: "s.r.tonoy.p@gmail.com", imageName: "envelope.circle")
+                InfoView(text: "GitHub - saifurrahmanproject", imageName: "icloud.and.arrow.up")
+                
+                
+            }
+        }
+        
     }
 }
 
@@ -19,3 +47,25 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct InfoView: View {
+    
+    var text: String
+    var imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 18)
+            .frame(height: 60.0)
+            .foregroundColor(.gray)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+
